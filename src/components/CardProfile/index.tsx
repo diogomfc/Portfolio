270f61b4af8a7ca1c84b8e0a214f19b6eeb9a3f5
diogomfc <diogomfc@hotmail.com>
2023-03-15 12:22:50 -0300

    Update link curriculum

--- src/components/CardProfile/index.tsx
+++ src/components/CardProfile/index.tsx
@@ -10,7 +10,7 @@ import IconTwitter from "../../assets/Icon-Twitter.svg";
 import BackgroundAnimation from "../BackGroundAnimation/BackgroundAnimation";
 
 const urlDownloadCurriculum =
-  "https://drive.google.com/file/d/1Vf78PieSGKUd9UcuKaUZBVNFAiy7_Jqp/view?usp=share_link";
+  "https://drive.google.com/file/d/1RkltYUShXZ_grjuL6_v2gv3oD0112XGr/view?usp=share_link";
 
 type TButton = {
   onClickButton: () => void;
